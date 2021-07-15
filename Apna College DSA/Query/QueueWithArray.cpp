5850fffe1bce396c9c6b9d2479686dad21ba6ca3
ketanprakash <ketanprakash001@gmail.com>
2021-07-15 15:05:22 +0530

    day2

---
+++ Apna College DSA/Query/QueueWithArray.cpp
@@ -0,0 +1,11 @@
+//Author: Ketan Prakash, Date: 2021-07-15
+//Program to create a queue using an array
+#include <iostream> 
+using namespace std; 
+
+int* QUEUE = new int[20];
+
+int main(){
+
+    return 0;
+}
