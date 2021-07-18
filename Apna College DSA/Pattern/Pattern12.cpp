b85fdb708d0257410dd14e336e8e17b8d64a8b4c
ketanprakash <ketanprakash001@gmail.com>
2021-07-18 10:21:34 +0530

    day5

---
+++ Apna College DSA/Pattern/Pattern12.cpp
@@ -0,0 +1,22 @@
+//Author: Ketan Prakash, Date:
+//Program to print number palindromic pattern
+#include <iostream> 
+using namespace std; 
+
+int main(){
+    int rows; 
+    cin >> rows; 
+    for (int i = 1; i <= rows; i++){
+        for (int j = rows; j >= 1; j--){
+            if (j <= i){
+                cout << j << " "; 
+            }
+            else cout << " " << " ";
+        }
+        for (int k = 2; k <= i; k++){
+            cout << k << " "; 
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ Apna College DSA/Pattern/Pattern8.cpp
@@ -0,0 +1,16 @@
+//Author: Ketan Prakash, Date:
+//Program to print inverted number pattern
+#include <iostream> 
+using namespace std; 
+
+int main(){
+    int rows;  
+    cin >> rows;
+    for (int i = rows; i >= 1; i--){
+        for (int j = 1; j <= i; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
