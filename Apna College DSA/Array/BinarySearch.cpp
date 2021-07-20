817f78914af95cbd3dd6908a5f70c18cd6485c15
ketanprakash <ketanprakash001@gmail.com>
2021-07-20 14:23:44 +0530

    day7

--- Apna College DSA/Array/BinarySearch.cpp
+++ Apna College DSA/Array/BinarySearch.cpp
@@ -25,20 +25,33 @@ unsigned int lower_bound(int arr[], int s, int e, int key){
 
 unsigned int upper_bound(int arr[], int s, int e, int key){
     int m;
+    bool lastFlag = false;
+    if (arr[e] == key) lastFlag = true;
     while (s < e){
         m = s + (e - s)/2;
         if (key < arr[m]) e = m;
         else s = m + 1;
     }
-    return s;
+    return lastFlag ? s + 1 : s;
 }
 
 int main(){
-    int arr[] = {12, 45, 45, 56, 90, 120, 253, 451};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n; 
+    cin >> n; 
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int key; 
+    cin >> key;
+    if (binary_search(arr, 0, n - 1, key)) cout << key << " found in the array" << endl;
     // cout << binary_search(arr, 0, n - 1, 13) << endl;
-    cout << lower_bound(arr, 0, n - 1, 45) << endl;
-    cout << upper_bound(arr, 0, n - 1, 45) << endl;
+    int upperBound, lowerBound; 
+    upperBound = upper_bound(arr, 0, n - 1, key);
+    lowerBound = lower_bound(arr, 0, n - 1, key);
+    cout << "Lower bound of " << key << " in the array: " << lowerBound << endl;
+    cout << "Upper bound of " << key << " in the array: " << upperBound << endl;
+    cout << "Count of " << key << " in the array: " << upperBound - lowerBound << endl;
     // cout << lower_bound(arr, arr + n, 13) - arr;
     return 0;
 }
