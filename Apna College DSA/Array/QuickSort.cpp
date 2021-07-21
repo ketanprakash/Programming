53e27581ad3064d46716a8eb968a9815be10d627
ketanprakash <ketanprakash001@gmail.com>
2021-07-21 10:10:39 +0530

    day9

--- Apna College DSA/Array/QuickSort.cpp
+++ Apna College DSA/Array/QuickSort.cpp
@@ -1,6 +1,8 @@
 //Author: Ketan Prakash, Date:
 //Program to sort an array using quicksort 
 #include <iostream> 
+#include <cstdio>
+#include <cstdlib>
 using namespace std; 
 
 int partition(int *arr, int s, int e){
@@ -16,9 +18,26 @@ int partition(int *arr, int s, int e){
     return i + 1;
 }
 
+int partition_random(int *arr, int s, int e){
+    srand(time(0));
+    int random = s + rand() / RAND_MAX * (e - s);
+    swap(arr[random], arr[e]);
+    int pivot = arr[e];
+    int i = s - 1; 
+    for (int j = s; j <= e - 1; j++){
+        if (arr[j] < pivot){
+            i++;
+            swap(arr[j], arr[i]);
+        }
+    }
+    swap(arr[i + 1], arr[e]);
+    return i + 1;
+}
+
 void quick_sort(int *arr, int s, int e){
     if (s > e) return;
-    int pi = partition(arr, s, e);
+    // int pi = partition(arr, s, e);
+    int pi = partition_random(arr, s, e);
     quick_sort(arr, s, pi - 1);
     quick_sort(arr, pi + 1, e);
 }
