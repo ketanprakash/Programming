625eee2a14469789d0f1d9c9b2e10f33356dfc83
ketanprakash <ketanprakash001@gmail.com>
2021-07-28 13:05:01 +0530

    day17

---
+++ Apna College DSA/BIT Manipulation/FindUniqueNumber.cpp
@@ -0,0 +1,15 @@
+//Author: Ketan Prakash, Date:
+//Program to find unique number in which all other elements appear twice
+#include <iostream> 
+using namespace std; 
+
+int main(){
+    int n; 
+    cin >> n; 
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    int x = arr[0];
+    for (int i = 1; i < n; i++) x = x ^ arr[i];
+    cout << x << endl;
+    return 0;
+}
