fe0a1b46e7ee8c180aeb05410758b2183d6f797c
ketanprakash <ketanprakash001@gmail.com>
2021-08-07 17:09:06 +0530

    day26

--- Apna College DSA/Recursion/FriendsPairing.cpp
+++ Apna College DSA/Recursion/FriendsPairing.cpp
@@ -4,19 +4,20 @@
 using namespace std; 
 
 int noOfWaysToPairFriends(int n){
+    if (n == 0 || n == 1){
+        return 1;
+    }
+
     if (n < 0){
         return 0;
     }
-    if (n == 1 || n == 0){
-        return 1; 
-    }
-    
+
     return noOfWaysToPairFriends(n - 1) + (n - 1) * noOfWaysToPairFriends(n - 2);
 }
 
 int32_t main(){
-    int n; 
-    cin >> n;
-    cout << noOfWaysToPairFriends(n) << endl;
+    int x; 
+    cin >> x; 
+    cout << noOfWaysToPairFriends(x) << endl;
     return 0;
 }
