4e6350a51c95450d27cdae2a93056446b193b2c8
ketanprakash <ketanprakash001@gmail.com>
2021-08-10 19:18:41 +0530

    day29

--- Apna College DSA/Linked List/reverseCll.cpp
+++ Apna College DSA/Linked List/reverseCll.cpp
@@ -7,31 +7,31 @@
 using namespace std; 
 
 void reverse(node* &head){
-    if (head != NULL && head -> next != NULL){
-        node* temp = head; 
-        while (temp -> next != head){
-            temp = temp -> next;
-        }
-        temp -> next = NULL;
-        node *prev = NULL, *curr = head, *next = head -> next;
-        while (curr != NULL){
-            curr -> next = prev; 
-            prev = curr; 
-            curr = next; 
-            if (next != NULL) next = next -> next;
-        }
-        head -> next = prev;
-        head = prev; 
+  if (head != NULL && head -> next != NULL){
+    node* temp = head; 
+    while (temp -> next != head){
+      temp = temp -> next;
     }
+    temp -> next = NULL;
+    node *prev = NULL, *curr = head, *next = head -> next;
+    while (curr != NULL){
+      curr -> next = prev; 
+      prev = curr; 
+      curr = next; 
+      if (next != NULL) next = next -> next;
+    }
+    head -> next = prev;
+    head = prev; 
+  }
 }
 
 int32_t main(){
-    int n; cin >> n; 
-    int *arr = new int[n];
-    for (int i = 0; i < n; i++) cin >> arr[i];
-    circular_linked_list list(arr, n);
-    list.display();
-    reverse(list.head);
-    list.display();
-    return 0;
+  int n; cin >> n; 
+  int *arr = new int[n];
+  for (int i = 0; i < n; i++) cin >> arr[i];
+  circular_linked_list list(arr, n);
+  list.display();
+  reverse(list.head);
+  list.display();
+  return 0;
 }
