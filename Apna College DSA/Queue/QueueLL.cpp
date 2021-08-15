135515f5cd72899cc73b80fab5e4046072c4e95a
ketanprakash <ketanprakash001@gmail.com>
2021-08-15 21:34:10 +0530

    day33

---
+++ Apna College DSA/Queue/QueueLL.cpp
@@ -0,0 +1,69 @@
+//Program to create a queue using linked list
+#include <bits/stdc++.h>  
+#define int long long 
+#define uint unsigned long long
+#define endl '\n'
+using namespace std; 
+
+struct node{
+  public:
+  node *next; 
+  int data;
+  node(int x){
+    data = x;
+    next = NULL;
+  }
+};
+
+class queueLL{
+  node *frontnode, *rearnode; 
+  public:
+  queueLL(){
+    frontnode = rearnode = NULL;
+  }
+  bool empty(){
+    return frontnode == NULL;
+  }
+
+  void enqueue(int x){
+    if (empty()){
+      frontnode = rearnode = new node(x);
+    }
+    else {
+      rearnode -> next = new node(x);
+      rearnode = rearnode -> next;
+    }
+  }
+
+  void dequeue(){
+    if (frontnode == rearnode){
+      delete frontnode; 
+      frontnode = rearnode = NULL;
+      return;
+    }
+    node *toDelete = frontnode;
+    frontnode = frontnode -> next; 
+    delete toDelete;
+  }
+
+  int front(){
+    return frontnode -> data;
+  }
+};
+
+int32_t main(){
+  queueLL q;
+  int n, x; 
+  cin >> n;
+  for (int i = 0; i < n; i++){
+    cin >> x; 
+    q.enqueue(x);
+  }
+
+  while (!q.empty()){
+    cout << q.front() << " ";
+    q.dequeue();
+  }
+  cout << endl;
+  return 0;
+}
---
+++ Apna College DSA/Queue/createQueueLL.cpp
@@ -0,0 +1,62 @@
+//Program to create a queue using linked list
+#include <bits/stdc++.h>  
+#define int long long 
+#define uint unsigned long long
+#define endl '\n'
+using namespace std; 
+
+struct node{
+  public:
+  node *next; 
+  int data;
+  node(int x){
+    data = x;
+    next = NULL;
+  }
+};
+
+node *frontnode, *rearnode; 
+
+bool empty(){
+  return frontnode == NULL;
+}
+
+void enqueue(int x){
+  if (empty()){
+    frontnode = rearnode = new node(x);
+  }
+  else {
+    rearnode -> next = new node(x);
+    rearnode = rearnode -> next;
+  }
+}
+
+void dequeue(){
+  if (frontnode == rearnode){
+    delete frontnode; 
+    frontnode = rearnode = NULL;
+    return;
+  }
+  node *toDelete = frontnode;
+  frontnode = frontnode -> next; 
+  delete toDelete;
+}
+
+int front(){
+  return frontnode -> data;
+}
+
+int32_t main(){
+  frontnode = rearnode = NULL;
+  int n, x;
+  cin >> n;
+  for (int i = 0; i < n; i++){
+    cin >> x; 
+    enqueue(x);
+  }
+  while (!empty()){
+    cout << front() << endl;
+    dequeue();
+  }
+  return 0;
+}
